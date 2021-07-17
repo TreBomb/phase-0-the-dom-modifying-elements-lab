fba0e6b68f2465dfc49e911c2139313bf93c0950
Trestin Ishak <trestinishak@gmail.com>
2021-07-17 09:04:05 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const main = document.getElementById('main');
+const newHeader = document.createElement('h1');
+
+main.remove();
+
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Trestin is the champion!'
